b278e587f3b7535ddd74eacd8cfd21b8bc8ad397
박종국 <whdrnrdl789@naver.com>
2024-07-31 19:38:16 +0900

    first commit

---
+++ src/main/java/com/example/wantedpreonboarding/WantedPreOnBoardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.wantedpreonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnBoardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedPreOnBoardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wantedpreonboarding/WantedPreOnBoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wantedpreonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnBoardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
