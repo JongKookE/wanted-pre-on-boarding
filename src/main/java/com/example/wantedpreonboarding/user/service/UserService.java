09c7341a9a22f9f036b9f32fad245fa6f30ae168
박종국 <whdrnrdl789@naver.com>
2024-08-01 20:02:00 +0900

    feat: Security 설정

--- src/main/java/com/example/wantedpreonboarding/user/service/UserService.java
+++ src/main/java/com/example/wantedpreonboarding/user/service/UserService.java
@@ -1,7 +1,31 @@
 package com.example.wantedpreonboarding.user.service;
 
+import com.example.wantedpreonboarding.user.dto.UserDto;
+import com.example.wantedpreonboarding.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class UserService {
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDto userDto = UserDto.builder()
+                .build();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        return userDto == null ? null : new User(userDto.getUserEmail(), userDto.getUserPassword(), authorities);
+    }
 }
