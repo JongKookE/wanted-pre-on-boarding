917a4e23f740495bff22e93c2823355d19afde41
박종국 <whdrnrdl789@naver.com>
2024-08-03 20:58:17 +0900

    feat: 유저 REST Controller

--- src/main/java/com/example/wantedpreonboarding/user/controller/UserControeller.java
+++ src/main/java/com/example/wantedpreonboarding/user/controller/UserControeller.java
@@ -1,9 +1,53 @@
 package com.example.wantedpreonboarding.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.wantedpreonboarding.user.dto.UserDto;
+import com.example.wantedpreonboarding.user.entity.UserEntity;
+import com.example.wantedpreonboarding.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
-@RequestMapping("users")
+@RequiredArgsConstructor
+@RequestMapping("api/users")
 public class UserControeller {
+    private final UserService userService;
+
+    @DeleteMapping
+    public ResponseEntity<UserEntity> delete(@RequestParam long userId) {
+        userService.softDeleteByUserId(userId);
+        // TODO 적절한 Response 코드 작성하기
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users?name={username}")
+    public ResponseEntity<UserDto> findByName(@PathVariable String username) {
+        return ResponseEntity.ok(userService.findByUserName(username));
+    }
+
+    @GetMapping("/users?email={userEmail}")
+    public ResponseEntity<UserDto> findByUserEmail(@PathVariable String userEmail) {
+        return ResponseEntity.ok(userService.findByUserEmail(userEmail));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok(userService.findUserAll());
+    }
+
+    @GetMapping("/users/deleted")
+    public ResponseEntity<List<UserDto>> findDeletedAll() {
+        return ResponseEntity.ok(userService.findDeletedUser());
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<UserDto> save(@RequestBody UserDto userDto) {
+        userService.userSave(userDto);
+        // TODO 적절한 Response 코드 작성하기
+        return ResponseEntity.ok().build();
+    }
+
 }
