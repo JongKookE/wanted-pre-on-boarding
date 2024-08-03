7daf6824bd83af091e83fe7615a11534936bbc5e
박종국 <whdrnrdl789@naver.com>
2024-08-03 20:58:49 +0900

    refactor: 사용하지 않는 import 삭제

--- src/main/java/com/example/wantedpreonboarding/config/SecurityConfig.java
+++ src/main/java/com/example/wantedpreonboarding/config/SecurityConfig.java
@@ -8,9 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -38,7 +36,6 @@ public class SecurityConfig {
                 // STATELESS 형태로 관리
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
-
     }
 
 
